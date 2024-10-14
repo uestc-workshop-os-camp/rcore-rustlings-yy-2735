360cf6318dce37eb20b54c111a9d8c902cde1d06
Yoyenne <2735730295@qq.com>
2024-10-14 10:06:21 +0800

    update

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -17,9 +17,9 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
-        if value < 1 {
+        if value < 0 {
             return Err(CreationError::Negative);
-        } else if value == 0 {
+        } else if value ==0 {
             return Err(CreationError::Zero);
         }
 
@@ -31,9 +31,13 @@ impl PositiveNonzeroInteger {
 fn test_creation() {
     assert!(PositiveNonzeroInteger::new(10).is_ok());
     assert_eq!(
-        Err(CreationError::Negative),
-        PositiveNonzeroInteger::new(-10)
+        PositiveNonzeroInteger::new(-10),
+        Err(CreationError::Negative)
+        
     );
     //
-    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+    assert_eq!(
+        PositiveNonzeroInteger::new(0),
+        Err(CreationError::Zero)
+    );
 }
